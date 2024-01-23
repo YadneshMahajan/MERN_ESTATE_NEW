1a79f4c350c58201f8b6b91d15e12c8d78f02885
yashodipmahajan <110547036+yashodipmahajan@users.noreply.github.com>
2024-01-23 11:01:55 +0530

    Sign In Page

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,16 +1,39 @@
-import User from "../models/user.model.js"
+import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
- 
-export const signup=async (req,res,next)=>{
-    const {username,email,password}=req.body;
-    const hashPassword=bcryptjs.hashSync(password,10);
-    const newUser=new User({username,email,password:hashPassword});
-try {
+import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
+
+export const signup = async (req, res, next) => {
+  const { username, email, password } = req.body;
+  const hashPassword = bcryptjs.hashSync(password, 10);
+  const newUser = new User({ username, email, password: hashPassword });
+  try {
     await newUser.save();
     res.status(201).json("user Created Successfully");
-} catch (error) {
-   next(error);
-}
-    
+  } catch (error) {
+    next(error);
+  }
+};
 
-}
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, "User not found!"));
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(404, "Wrong Credentials !!"));
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    
+    const {password: pass, ...rest}=validUser._doc;
+    res.cookie("access_token", token, {
+        httpOnly: true,
+        expires: new Date(Date.now() + 24 * 60 * 69 * 1000),
+      })
+      .status(200)
+      .json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
